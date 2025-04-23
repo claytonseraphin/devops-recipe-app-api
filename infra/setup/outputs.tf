332bdc2f142a63c4f5d684b59627cb59db49c33b
Claytonseraphin <clayton0687@gmail.com>
2025-04-22 22:03:19 -0400

    Added ecr setup to terraform

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -8,3 +8,13 @@ output "cd_user_access_key_secret" {
   value       = aws_iam_access_key.cd.secret
   sensitive   = true
 }
+
+output "aws_ecr_app" {
+  description = "ECR repository url for app image"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+output "aws_ecr_proxy" {
+  description = "ECR repository url for proxy image"
+  value       = aws_ecr_repository.proxy.repository_url
+}
